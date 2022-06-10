25ce9cfa35e94690695d6a3c10c83abf027e66e5
suiorkul <58985314+suiorkul@users.noreply.github.com>
2022-06-10 20:28:48 +0600

    Add files via upload

---
+++ week3/Sales.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Sales {
+    public long getId() {
+        return id;
+
+    }
+
+    public Sales setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNameOfCar() {
+        return nameOfCar;
+    }
+
+    public Sales setNameOfCar(String nameOfCar) {
+        this.nameOfCar = nameOfCar;
+        return this;
+    }
+
+    private long id;
+    private String nameOfCar;
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public Sales setEmployer(Employer employer) {
+        this.employer = employer;
+        return this;
+    }
+
+    private Employer employer;
+
+    public Sales(String nameOfCar) {
+        this.nameOfCar = nameOfCar;
+    }
+
+
+
+
+}
